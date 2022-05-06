5d3471f9671a11a1e603e76e9a8016115611c4a2
Joanna <joanna.szczerbowska@gmail.com>
2022-05-06 14:02:36 +0200

    Operations

--- budgetapp/src/js/OperationsCard/OperationsCard.js
+++ budgetapp/src/js/OperationsCard/OperationsCard.js
@@ -21,25 +21,119 @@ const OperationsCard = () => {
         
         <div className="card">
             <TimePeriod className="time-period"/>
-            <div className="expenses-list__card">
-                <div className="expenses-list__header">
+
+            {/* <table className={"operations__table"}>
+                <thead>
+                    <tr className="table__header">
+                        <th>Data</th>
+                        <th>Kategoria</th>
+                        <th>Płatnik</th>
+                        <th>Kwota</th>
+                        <th>Akcje</th>
+                    </tr>
+                </thead>
+                <tbody className="table__body">
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr><tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                    <tr>
+                        <td>2022-05-12</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>500</td>
+                        <td>Edytuj Usuń</td>
+                    </tr>
+                </tbody>
+            </table> */}
+
+            <div className="operations-list__card">
+                <div className="operations-list__header">
                     <div>Operacje</div>
+                    <div>Płatnik</div>
                     <div>Kwota</div>
+                    <div>Akcje</div>
                 </div>
 
-                <div className="expenses-list__list">
+                <div className="operations-list__list">
                 {
                     operationsData?.map(({ id, categoryID, subcategoryID, amount, date}) => (
-                    <div key = {id} className="expenses-list__subcategory">
-                        <div className="expenses-list__subcategory--name">{date}</div>
+                    <div key = {id} className="operations-list__category">
+                        <div className="operations-list__date">{date}</div>
                         <div>
-                            <div className="expenses-list__category--name">{handleCategory(categoryID, expensesData)}</div>
-                            <div className="expenses-list__subcategory--name">{handleSubcategory(subcategoryID, categoryID, expensesData)}</div>
+                            <div className="operations-list__category--name">{handleCategory(categoryID, expensesData)}</div>
+                            <div className="operations-list__subcategory--name">{handleSubcategory(subcategoryID, categoryID, expensesData)}</div>
                         </div>
-                        <div className="expenses-list__subcategory--sum"><span>-{amount}</span> zł 
+                        <div className="operations-list__amount"><span>-{amount}</span> zł 
                         </div>
-                        <button className="btn">Edytuj</button>
+                        <div>
                         <button className="btn">Usuń</button>
+                        </div>
                     </div>
                     ))
                 }
